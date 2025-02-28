bf58e699148be2fb0125a03503a6b6d9bc9e050a
fortmindzsubhadip <subahdip.shee@fortmindz.in>
2025-02-28 15:54:55 +0530

    basic redis understand done

--- src/index.js
+++ src/index.js
@@ -18,19 +18,51 @@ const testRedis = async () => {
 // let my_name= await client.set('name','subhadip shee');
 // let getMyName=await client.get('name')
 
-await client.mSet(["user:name", "subhadip shee", "user:email", "subhadipshee2001@gmail.com"]);
+// await client.mSet(["user:name", "subhadip shee", "user:email", "subhadipshee2001@gmail.com"]);
 
-// Correct way to get multiple values
-const [name] = await client.mGet(["user:name"]);
+// // Correct way to get multiple values
+// const [name,email] = await client.mGet(["user:name","user:email"]);
 
-console.log(name); 
+// console.log(name,email); 
 
-
-
-
+// list data structure LPUSH ,RPUSH ,LPOP ,RPOP ,LRANGE 
 
 
-   
+
+// await client.lPush('skills',['nodejs','expressjs','redis','mongodb']);
+// const getAllSkill=await client.lRange('skills',0,-1);
+// // console.log(getAllSkill);
+
+//    const getFirstSkill=await client.lPop('skills');
+//    const getLastSkill=await client.rPop('skills');
+//    console.log(getFirstSkill,getLastSkill);
+
+
+// now its comes  to work with set data structure. 
+// Methods are SADD ,SMEMBERS ,SISMEMBER ,SREM
+
+let addSkillls=await client.sAdd('skills',['react','expressjs','nodejs','mongodb']);
+console.log(addSkillls)
+const getAllSkill=await client.sMembers('skills');
+console.log(getAllSkill);
+const remSkill=await client.sRem('skills','react');
+console.log(getAllSkill);
+console.log(remSkill);
+
+// make a pub sub-model
+// Create a new Redis client
+
+const subscriber = client.duplicate();
+await subscriber.connect();
+
+// Subscribe to a channel
+await subscriber.subscribe("message", (message) => {
+  console.log("Received message:", message);
+});
+
+// Publish a message to the channel
+const publishCount = await client.publish("message", "hello world");
+console.log("Number of subscribers received the message:", publishCount);
   } catch (error) {
     console.log(error);
   } finally {
@@ -45,3 +77,5 @@ app.listen(3000, () => {
 module.exports = {
   client,
 };
+
+
